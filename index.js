3e2dcfb69c4a4a40e698998491001ccd16ffa8f7
Nehalkothale <107396461+Nehalkothale@users.noreply.github.com>
2024-08-27 14:46:42 +0530

    basics

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Revision of the node")
+// console.log("Revision of the node")
+// var a=10
+// var b=20
+// var c=30
+
+// console.log(a+b+c);
+
+
+//BASIC 
+//if else
+// let age=10;
+// if(age>10){
+//     console.log("True");
+// }else{
+//     console.log("Flase");
+// }
+
